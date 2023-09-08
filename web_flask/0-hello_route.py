18f261db8b00e2e829dbbd41c2228cd360e27d84
Sir-Blow <emmanuelakej@gmail.com>
2023-09-08 22:11:55 +0000

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env bash
+"""#!/usr/bin/python3"""
 """
 This script starts a Flask web application:
 That listens on 0.0.0.0, port 5000
 Routes:
-/: display “Hello HBNB!”
+/: display "Hello HBNB!"
 You must use the option strict_slashes=False in your route definition
 """
 
-from flask import Flask,
+from flask import Flask
 
-app = Flask(__name__)
+app=Flask"(__name__)"
 
 
-@app.route("/airbnb-onepage/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """This displays 'Hello HBNB!'"""
     return "Hello HBNB!"
