3b75690ca7673c6821b0129084e955c529c58cfb
Sir-Blow <emmanuelakej@gmail.com>
2023-09-08 22:55:56 +0000

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,15 +6,16 @@ Routes:
 /: display "Hello HBNB!"
 You must use the option strict_slashes=False in your route definition
 """
-from flask import Flask
-app = Flask"(__name__)"
+from flask import Flask, render_template
+
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
 def hello():
     """This displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    return render_template("10-hbnb_filters.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=none)
